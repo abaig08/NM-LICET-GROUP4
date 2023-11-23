c0f3ef16bf2264718e04f1929a13743e593ef154
Paul-M-Kallarackal <paulmkallarackal@gmail.com>
2023-11-23 20:31:45 +0530

    SpringBoot Initialization

---
+++ Milestone4-Backend/wta/src/test/java/com/wta/wta/WtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wta.wta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
